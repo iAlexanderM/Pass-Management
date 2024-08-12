740ee6df8c5c8f9fca1ca63f5bee7c18c0fc5718
Alexander Mikhailov <zelomod@mail.ru>
2024-08-12 19:07:34 +0300

    update before snos

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,30 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { PersonCreateComponent } from './person-create/person-create.component';
-import { PersonListComponent } from './person-list/person-list.component';
-import { PersonDetailsComponent } from './person-details/person-details.component';
-import { CameraModalComponent } from './camera-modal/camera-modal.component';
+//import { PersonCreateComponent } from './person-create/person-create.component';
+//import { PersonListComponent } from './person-list/person-list.component';
+//import { PersonDetailsComponent } from './person-details/person-details.component';
+///*import { CameraModalComponent } from './camera-modal/camera-modal.component';*/
 import { CommonModule } from '@angular/common';
 
-const appRoutes: Routes = [
-  { path: 'persons', component: PersonListComponent },
-  { path: 'persons/create', component: PersonCreateComponent },
-  { path: 'persons/:id', component: PersonDetailsComponent }
-];
+//const appRoutes: Routes = [
+//  { path: 'persons', component: PersonListComponent },
+//  { path: 'persons/create', component: PersonCreateComponent },
+//  { path: 'persons/:id', component: PersonDetailsComponent }
+//];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonCreateComponent,
-    PersonListComponent,
-    PersonDetailsComponent,
-    CameraModalComponent
+//    PersonCreateComponent,
+//    PersonListComponent,
+//    PersonDetailsComponent,
+///*    CameraModalComponent*/
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes),
+/*    RouterModule.forRoot(appRoutes),*/
     CommonModule
   ],
   providers: [],
--- obj/Debug/net8.0/PassManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/PassManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PassManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8f7e765fc0b8f9cc07d9b5d737bd34dd66a6959")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6264ac8dbd3df0145471acd8ba148a664091c7cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("PassManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PassManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
